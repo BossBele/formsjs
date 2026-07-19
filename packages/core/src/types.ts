2a1668b430a0e97dc7be1146013eab4a3a9592ae
Adam Beleko <bossbele@larven.io>
2026-07-19 22:19:48 +0300

    chore: bump version to 0.6.0

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -17,13 +17,20 @@ export interface Dependency {
   type: DependencyType;
 }
 
+export type ValidationValue = boolean | number | string | RegExp;
+export type ValidationRule<TValidationValue extends ValidationValue = ValidationValue> = TValidationValue | ValidationValueMessage<TValidationValue>;
+export type ValidationValueMessage<TValidationValue extends ValidationValue = ValidationValue> = {
+  value: TValidationValue;
+  message: string;
+};
+
 export interface ValidationRules {
-  required?: boolean;
-  min?: number;
-  max?: number;
-  minLength?: number;
-  maxLength?: number;
-  pattern?: string | RegExp;
+  required?: string | ValidationRule<boolean>;
+  min?: ValidationRule<number | string>;
+  max?: ValidationRule<number | string>;
+  maxLength?: ValidationRule<number>;
+  minLength?: ValidationRule<number>;
+  pattern?: string | RegExp | ValidationRule<RegExp | string>;
   disabled?: boolean;
   readonly?: boolean;
 }
